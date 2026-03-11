5e70696e3db24b3d2fcfd95d7b9d0cd2d901d9fe
sameer76-byte <sameerprasad981@gmail.com>
2026-03-11 14:10:57 +0530

    basic variable I/O

---
+++ 01-basic/01-variables-datatypes/main.java
@@ -0,0 +1,15 @@
+public class main{
+    public static void main(String[] args) {
+        // Variables
+        String name = "John";
+        int age = 30;
+        double height = 1.75;
+        boolean isStudent = true;
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
